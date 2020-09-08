02b2d27fb6fafd70de8b42ef7b4bc51f9406b53f
Sirwan Afifi <sir1afifi@gmail.com>
2020-09-08 14:00:00 +0100

    Add Intl date time

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./tailwind.output.css";
 
-export const App = () => (
-  <div className="max-w-md mx-auto flex p-6 bg-gray-100 mt-10 rounded-lg shadow-xl">
-    <div className="ml-6 pt-1">
-      <h1 className="text-2xl text-blue-700 leading-tight">
-        Tailwind and Create React App
-      </h1>
-      <p className="text-base text-gray-700 leading-normal">
-        Building apps together
-      </p>
+export const App = () => {
+  const [time, setTime] = useState<number>();
+  useEffect(() => {
+    let timer: any;
+    timer = setInterval(() => {
+      setTime(new Date().getTime());
+    }, 1000);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
+  return (
+    <div className="max-w-md mx-auto flex p-6 bg-gray-100 mt-10 rounded-lg shadow-xl">
+      <div className="ml-6 pt-1">
+        <h1 className="text-2xl text-blue-700 leading-tight">
+          {new Intl.DateTimeFormat("en-US", {
+            hour: "2-digit",
+            minute: "2-digit",
+            second: "2-digit"
+          }).format(time)}
+        </h1>
+      </div>
     </div>
-  </div>
-);
+  );
+};
